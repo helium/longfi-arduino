e6150101c9c6cc313286a66cdd1c3081e906fd53
Addy Gallareta <addy.gallareta@gmail.com>
2020-02-06 16:44:45 -0800

    remove boards not tested, changed interval

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -61,7 +61,7 @@ static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 60;
+const unsigned TX_INTERVAL = 10;
 
 // Pin mapping
 //
@@ -79,34 +79,7 @@ const lmic_pinmap lmic_pins = {
     .rssi_cal = 8,              // LBT cal for the Adafruit Feather M0 LoRa, in dB
     .spi_freq = 8000000,
 };
-#elif defined(ARDUINO_AVR_FEATHER32U4)
-// Pin mapping for Adafruit Feather 32u4 LoRa, etc.
-// Just like Feather M0 LoRa, but uses SPI at 1MHz; and that's only
-// because MCCI doesn't have a test board; probably higher frequencies
-// will work.
-const lmic_pinmap lmic_pins = {
-    .nss = 8,
-    .rxtx = LMIC_UNUSED_PIN,
-    .rst = 4,
-    .dio = {7, 6, LMIC_UNUSED_PIN},
-    .rxtx_rx_active = 0,
-    .rssi_cal = 8,              // LBT cal for the Adafruit Feather 32U4 LoRa, in dB
-    .spi_freq = 1000000,
-};
-#elif defined(ARDUINO_CATENA_4551)
-// Pin mapping for Murata module / Catena 4551
-const lmic_pinmap lmic_pins = {
-        .nss = 7,
-        .rxtx = 29,
-        .rst = 8,
-        .dio = { 25,    // DIO0 (IRQ) is D25
-                 26,    // DIO1 is D26
-                 27,    // DIO2 is D27
-               },
-        .rxtx_rx_active = 1,
-        .rssi_cal = 10,
-        .spi_freq = 8000000     // 8MHz
-};
+
 #elif defined(MCCI_CATENA_4610) 
 #include "arduino_lmic_hal_boards.h"
 const lmic_pinmap lmic_pins = *Arduino_LMIC::GetPinmap_Catena4610();
@@ -170,7 +143,7 @@ void onEvent (ev_t ev) {
             }
             // Disable link check validation (automatically enabled
             // during join, but because slow data rates change max TX
-        // size, we don't use it in this example.
+            // size, we don't use it in this example.
             LMIC_setLinkCheckMode(0);
             break;
         /*
@@ -272,7 +245,6 @@ void do_send(osjob_t* j){
             payload[idx] = 0;
           }
         }
-        //LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
         Serial.println(F("Packet queued"));
         LMIC_setTxData2(1, payload, idx, 0);  
     }
